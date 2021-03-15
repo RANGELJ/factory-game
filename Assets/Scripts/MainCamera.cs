b16591c9b0237eca69423ef50b545869146d2dea
Jorge Rangel <jorgerangel@Jorges-Mac-mini.local>
2021-03-14 20:25:28 -0600

    No drag camera over ui

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MainCamera : MonoBehaviour {
     public float speed = 0.01F;
 
     void Update() {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
-            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-            transform.Translate(
-                -touchDeltaPosition.x * speed,
-                -touchDeltaPosition.y * speed,
-                0
-            );
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+
+            if (
+                !EventSystem.current.IsPointerOverGameObject(touch.fingerId)
+                && touch.phase == TouchPhase.Moved
+            ) {
+                Vector2 touchDeltaPosition = touch.deltaPosition;
+                transform.Translate(
+                    -touchDeltaPosition.x * speed,
+                    -touchDeltaPosition.y * speed,
+                    0
+                );
+            }
         }
     }
 }
